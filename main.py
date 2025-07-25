3ea72a77c8a4c1286f75c5a6ab71d321d66adaef
GlebFr <you git config --global user.email you>
2025-07-25 18:01:32 +0300

    телеграм бот начал повторять мои сообщения

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-
-from telegram import ForceReply
-from telegram.txt import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
+    await update.message.reply_text('hi my')
 
 
 async def echo (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.replay_text(update.message.txt)
+    await update.message.reply_text(update.message.text)
 
 
 def main():
-    app = Application.builder().token("TOKEN").build()
+    app = Application.builder().token("7257720687:AAH0oZF4smufI6b4va2tDbEyNpDTwz3kaEg").build()
     app.add_handler(CommandHandler('start', start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 main()
