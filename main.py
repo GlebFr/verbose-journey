50bfb8fb758aa836637e0e336da04e89c886eb57
GlebFr <you git config --global user.email you>
2025-07-30 19:07:55 +0300

    импортировали токен в файл .env

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from dotenv import load_dotenv
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-
+import os
 
 load_dotenv()
 
@@ -16,7 +16,7 @@ async def echo (update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 def main():
-    app = Application.builder().token("7257720687:AAH0oZF4smufI6b4va2tDbEyNpDTwz3kaEg").build()
+    app = Application.builder().token(os.getenv('BOT_TOKEN')).build()
     app.add_handler(CommandHandler('start', start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     app.run_polling(allowed_updates=Update.ALL_TYPES)
