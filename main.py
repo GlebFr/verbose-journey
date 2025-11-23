8bf0ba2b6daf9baf67be5eedf2e0603e5fff654c
GlebFr <you git config --global user.email you>
2025-11-23 20:01:09 +0300

    проверка даты в формате дд.мм.гггг

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import logging
+from datetime import datetime
 from dotenv import load_dotenv
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes, ConversationHandler, MessageHandler, filters
@@ -27,7 +28,7 @@ async def choose_type(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await query.edit_message_reply_markup(reply_markup=None)
     keyboard = [
         [
-            InlineKeyboardButton("однаразавая", callback_data="2_1"),
+            InlineKeyboardButton("однoразавая", callback_data="2_1"),
             InlineKeyboardButton("повторна", callback_data="2_2"),
             ],
     ]
@@ -63,15 +64,33 @@ async def choose_type_handler(update: Update, context: ContextTypes.DEFAULT_TYPE
 
 
 async def get_notification_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    context.user_data['name'] = update.message.text
-    await update.message.reply_text(f'ваше напоминание сохранино: {update.message.text} спасибо.\n теперь напишите время отправки напоминания')
+    context.user_data['notification_text'] = update.message.text
+    await update.message.reply_text(f'ваше напоминание сохранeно: {update.message.text} спасибо.\n теперь напишите время отправки напоминания в формате дд.мм.гггг')
     return NOTIFICATION_SEND_TIME
 
 
+def check_data(message): # данна функция принемает обЪект message 
+    try:
+        user_data = datetime.strptime(message, '%d.%m.%Y').date() #  провиряет на д.м.г
+        now_data = datetime.now().date() # берет время с компьютера
+        if user_data > now_data: # сравнивает дату и дату компа
+            return True
+        else:
+            return False
+
+
+    
+    except ValueError:
+        return False
+
 
 async def notifaction_send_time(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    context.user_data['name'] = update.message.text
-    await update.message.reply_text(f'ваше напоминание будет сохранино {update.message.text}')
+    notification_text = context.user_data['notification_text']
+    if check_data(update.message.text):
+        await update.message.reply_text(f'ваше напоминание {notification_text}, будет отправлено {update.message.text}')
+    else:
+        await update.message.reply_text(f'Пожалуйста введите в формате дд.мм.гггг')
+        return NOTIFICATION_SEND_TIME
     keyboard = [
         [
             InlineKeyboardButton("Начнем", callback_data="1"),
@@ -83,6 +102,10 @@ async def notifaction_send_time(update: Update, context: ContextTypes.DEFAULT_TY
     await update.message.reply_text("Привет, этот бот поможет тебе делать заметки, хочешь ли ты начать?", reply_markup=reply_markup)
     print(context.user_data)
     return CHOOSE_TYPE
+
+
+
+
 
 
 
